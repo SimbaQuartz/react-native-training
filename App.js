e411fb6e93c297b515666d9dd531adf12e09f823
Ansh Gujral <anshgujral1997@gmail.com>
2018-12-26 20:05:24 +0530

    inserting text inside view

--- App.js
+++ App.js
@@ -9,9 +9,15 @@ export default class App extends React.Component {
     return(
       <View style={{height, width}}>
         <View style={{backgroundColor: 'red', flex:1, flexDirection: 'row'}}>
-          <View style={{backgroundColor: 'green', flex:1}}/>
-          <View style={{backgroundColor: 'blue', flex:1}}/>
-          <View style={{backgroundColor: 'violet', flex:1}}/>
+          <View style={{backgroundColor: 'green', flex:1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={{fontSize: 20, color: 'white'}}>1</Text>
+          </View>
+          <View style={{backgroundColor: 'blue', flex:1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={{fontSize: 20, color: '#fff'}}>1</Text>
+          </View>
+          <View style={{backgroundColor: 'violet', flex:1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={{fontSize: 20, color: 'rgba(255, 255, 255, 0.5)'}}>1</Text>
+          </View>
         </View>
         <View style={{backgroundColor: 'orange', flex:1, flexDirection: 'row'}}>
           <View style={{backgroundColor: 'black', flex:1}}/>
