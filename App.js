c1146eb1933d82238b4e63801f6b655ee8ce831d
Ansh Gujral <anshgujral1997@gmail.com>
2018-12-26 18:27:42 +0530

    flex design
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

--- App.js
+++ App.js
@@ -8,9 +8,26 @@ export default class App extends React.Component {
   render() {
     return(
       <View style={{height, width}}>
-        <View style={{backgroundColor: 'red', flex:1}}/>
-        <View style={{backgroundColor: 'green', flex:1}}/>
-        <View style={{backgroundColor: 'blue', flex:1}}/>
+        <View style={{backgroundColor: 'red', flex:1, flexDirection: 'row'}}>
+          <View style={{backgroundColor: 'green', flex:1}}/>
+          <View style={{backgroundColor: 'blue', flex:1}}/>
+          <View style={{backgroundColor: 'violet', flex:1}}/>
+        </View>
+        <View style={{backgroundColor: 'orange', flex:1, flexDirection: 'row'}}>
+          <View style={{backgroundColor: 'black', flex:1}}/>
+          <View style={{backgroundColor: 'pink', flex:1}}/>
+          <View style={{backgroundColor: 'yellow', flex:1}}/>
+        </View>
+        <View style={{backgroundColor: 'red', flex:1, flexDirection: 'row'}}>
+          <View style={{backgroundColor: 'green', flex:1}}/>
+          <View style={{backgroundColor: 'blue', flex:1}}/>
+          <View style={{backgroundColor: 'violet', flex:1}}/>
+        </View>
+        <View style={{backgroundColor: 'orange', flex:1, flexDirection: 'row'}}>
+          <View style={{backgroundColor: 'black', flex:1}}/>
+          <View style={{backgroundColor: 'pink', flex:1}}/>
+          <View style={{backgroundColor: 'yellow', flex:1}}/>
+        </View>
       </View>
     )
   }
