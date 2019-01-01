2cd9c1fc48f600ca9ac93f0be5c95309a098cb17
Ansh Gujral <anshgujral1997@gmail.com>
2019-01-01 17:47:22 +0530

    sync async explained

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import {View, Text, TouchableOpacity} from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import { Dimensions } from 'react-native'
 
-const {height, width} = Dimensions.get('window')
+const { height, width } = Dimensions.get('window')
 
 export default class App extends React.Component {
 
@@ -12,39 +12,43 @@ export default class App extends React.Component {
   }
 
   render() {
-    return(
-      <View style={{height, width}}>
-        <View style={{flex:1, flexDirection: 'row'}}>
-          <TouchableOpacity style={{backgroundColor: 'green', flex:1, alignItems: 'center', justifyContent: 'center'}}
-          onPress={()=>this.setState({
-            text: this.state.text+1
-          })}>
-          <Text style={{fontSize: 20, color: 'white'}}>{this.state.text}</Text>
+    return (
+      <View style={{ height, width }}>
+        <View style={{ flex: 1, flexDirection: 'row' }}>
+          <TouchableOpacity style={{ backgroundColor: 'green', flex: 1, alignItems: 'center', justifyContent: 'center' }}
+            onPress={async () => {
+              await this.setState({
+                text: this.state.text + 1
+              })
+              console.log(this.state.text)
+            }
+            }>
+            <Text style={{ fontSize: 20, color: 'white' }}>{this.state.text}</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={{backgroundColor: 'green', flex:1, alignItems: 'center', justifyContent: 'center'}}
-          onPress={()=>this.setState({
-            text: this.state.text+1
-          })}>
-          <Text style={{fontSize: 20, color: 'white'}}>{this.state.text}</Text>
+          <TouchableOpacity style={{ backgroundColor: 'green', flex: 1, alignItems: 'center', justifyContent: 'center' }}
+            onPress={() => this.setState({
+              text: this.state.text + 1
+            })}>
+            <Text style={{ fontSize: 20, color: 'white' }}>{this.state.text}</Text>
           </TouchableOpacity>
-          <View style={{backgroundColor: 'violet', flex:1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text style={{fontSize: 20, color: 'rgba(255, 255, 255, 0.5)'}}>1</Text>
+          <View style={{ backgroundColor: 'violet', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ fontSize: 20, color: 'rgba(255, 255, 255, 0.5)' }}>1</Text>
           </View>
         </View>
-        <View style={{backgroundColor: 'orange', flex:1, flexDirection: 'row'}}>
-          <View style={{backgroundColor: 'black', flex:1}}/>
-          <View style={{backgroundColor: 'pink', flex:1}}/>
-          <View style={{backgroundColor: 'yellow', flex:1}}/>
+        <View style={{ backgroundColor: 'orange', flex: 1, flexDirection: 'row' }}>
+          <View style={{ backgroundColor: 'black', flex: 1 }} />
+          <View style={{ backgroundColor: 'pink', flex: 1 }} />
+          <View style={{ backgroundColor: 'yellow', flex: 1 }} />
         </View>
-        <View style={{backgroundColor: 'red', flex:1, flexDirection: 'row'}}>
-          <View style={{backgroundColor: 'green', flex:1}}/>
-          <View style={{backgroundColor: 'blue', flex:1}}/>
-          <View style={{backgroundColor: 'violet', flex:1}}/>
+        <View style={{ backgroundColor: 'red', flex: 1, flexDirection: 'row' }}>
+          <View style={{ backgroundColor: 'green', flex: 1 }} />
+          <View style={{ backgroundColor: 'blue', flex: 1 }} />
+          <View style={{ backgroundColor: 'violet', flex: 1 }} />
         </View>
-        <View style={{backgroundColor: 'orange', flex:1, flexDirection: 'row'}}>
-          <View style={{backgroundColor: 'black', flex:1}}/>
-          <View style={{backgroundColor: 'pink', flex:1}}/>
-          <View style={{backgroundColor: 'yellow', flex:1}}/>
+        <View style={{ backgroundColor: 'orange', flex: 1, flexDirection: 'row' }}>
+          <View style={{ backgroundColor: 'black', flex: 1 }} />
+          <View style={{ backgroundColor: 'pink', flex: 1 }} />
+          <View style={{ backgroundColor: 'yellow', flex: 1 }} />
         </View>
       </View>
     )
