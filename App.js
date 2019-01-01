e3ef649e240aa4481391bfdd2548694a00fdb98d
Ansh Gujral <anshgujral1997@gmail.com>
2019-01-01 17:35:57 +0530

    state introduction

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, TouchableOpacity} from 'react-native'
 import { Dimensions } from 'react-native'
 
 const {height, width} = Dimensions.get('window')
 
 export default class App extends React.Component {
+
+  state = {
+    text: 1,
+    text2: 'B'
+  }
+
   render() {
     return(
       <View style={{height, width}}>
-        <View style={{backgroundColor: 'red', flex:1, flexDirection: 'row'}}>
-          <View style={{backgroundColor: 'green', flex:1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text style={{fontSize: 20, color: 'white'}}>1</Text>
-          </View>
-          <View style={{backgroundColor: 'blue', flex:1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text style={{fontSize: 20, color: '#fff'}}>1</Text>
-          </View>
+        <View style={{flex:1, flexDirection: 'row'}}>
+          <TouchableOpacity style={{backgroundColor: 'green', flex:1, alignItems: 'center', justifyContent: 'center'}}
+          onPress={()=>this.setState({
+            text: this.state.text+1
+          })}>
+          <Text style={{fontSize: 20, color: 'white'}}>{this.state.text}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{backgroundColor: 'green', flex:1, alignItems: 'center', justifyContent: 'center'}}
+          onPress={()=>this.setState({
+            text: this.state.text+1
+          })}>
+          <Text style={{fontSize: 20, color: 'white'}}>{this.state.text}</Text>
+          </TouchableOpacity>
           <View style={{backgroundColor: 'violet', flex:1, alignItems: 'center', justifyContent: 'center'}}>
             <Text style={{fontSize: 20, color: 'rgba(255, 255, 255, 0.5)'}}>1</Text>
           </View>
