fcb1f6f72238ba70b1782103a1fb19fbe55c1586
Ansh Gujral <anshgujral1997@gmail.com>
2019-01-01 19:19:12 +0530

    application of maps, state and props(select one of 3 name) explained
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
 import React from 'react'
 import Main, { a, b } from './src/Main'
+import { View, Text, TouchableOpacity } from 'react-native'
+
 export default class App extends React.Component {
 
+  state = {
+    selected: 0
+  }
+
   render() {
-    console.log(a)
+    const a = [
+      {
+        name: 'Radhika'
+      },
+      {
+        name: 'Joban'
+      },
+      {
+        name: 'Agam'
+      }
+    ]
     return (
-      <Main
-        name="Hello World"
-        color="red"
-      />
+      <View>
+        {
+          a.map((object, key) => {
+            return (
+              <TouchableOpacity key={key} onPress={() => this.setState({ selected: key })}>
+                <Main
+                  name={object.name}
+                  color={this.state.selected === key ? 'green' : 'red'}
+                >
+                  {console.log(object.name, key)}
+                </Main>
+              </TouchableOpacity>
+            )
+          })
+        }
+      </View>
     )
   }
 }
