264182836f33d48ac9cd301a39df3fe29e544299
Ansh Gujral <anshgujral1997@gmail.com>
2019-01-01 19:27:31 +0530

    added src

---
+++ src/Main.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { Dimensions } from 'react-native'
+
+const { height, width } = Dimensions.get('window')
+
+export default class Main extends React.Component {
+    state = {
+        text: 1
+    }
+    render() {
+        return (
+            <Text style={{ color: this.props.color, margin: 100, fontSize: 30 }}>{this.props.name}</Text>
+        )
+    }
+}
+const a = 0
+const b = 0
+
+export { a, b } 
